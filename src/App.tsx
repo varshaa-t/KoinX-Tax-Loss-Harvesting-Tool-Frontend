184b05778051bd42531351f4b6384ef65de26dd6
Varshaa <vvarshaas@gmail.com>
2025-05-19 16:20:08 +0530

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='bg-primary-gray font-inter h-screen'>
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+  return (
+    <div className='bg-white py-2 px-10 sm:px-12 md:px-20'>
+        <img src='/images/nav-icon.png'/>
+    </div>
+  )
+}
+
+export default Navbar
