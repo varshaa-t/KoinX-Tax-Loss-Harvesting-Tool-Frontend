6e6141feeb1c86f73ec0cfff5236e0571acf93be
Varshaa <vvarshaas@gmail.com>
2025-05-21 11:59:57 +0530

    Navbar minor updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 
 function Navbar() {
   return (
-    <div className='bg-white py-2 px-10 sm:px-12 md:px-20'>
+    <div className='bg-white px-10 sm:px-12 md:px-20'>
         <img src='/images/nav-icon.png'/>
     </div>
   )
